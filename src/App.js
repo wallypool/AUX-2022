d010c28b345f448e27090eca8d7e49811f5439c7
wallypool <felix74gato@gmail.com>
2022-06-07 22:21:28 +0000

    example-0_settime

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+function getTime() {
+  return <div>son las {new Date().toLocaleTimeString()}</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edssdasdwsdasdit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [Time, setTime] = useState(getTime());
+  setInterval(() => {
+    setTime(getTime());
+  }, 1000);
+  return <div className="App">Hola mundo react 2022 {Time}</div>;
 }
 
 export default App;
